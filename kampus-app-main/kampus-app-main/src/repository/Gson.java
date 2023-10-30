dc7bc41822f22c5aab05a5c289c2090e8c179602
Danielsebastian11232 <96022916+Danielsebastian11232@users.noreply.github.com>
2023-10-30 21:28:44 +0700

    Add files via upload

---
+++ kampus-app-main/kampus-app-main/src/repository/Gson.java
@@ -0,0 +1,15 @@
+package repository;
+
+import model.DBTables;
+
+public class Gson {
+
+    public char[] toJson(DBTables tables) {
+        return null;
+    }
+
+    public DBTables fromJson(String jsonString, Class<DBTables> class1) {
+        return null;
+    }
+
+}
